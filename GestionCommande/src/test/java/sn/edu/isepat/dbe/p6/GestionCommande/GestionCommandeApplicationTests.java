23dbe7a9bd303d2d1aab8e23f21614c64838ffcf
MiaDev4 <ndeyeamyciss2@gmail.cocm>
2026-05-22 16:22:26 +0000

    initialisation et configuration des profiles

---
+++ GestionCommande/src/test/java/sn/edu/isepat/dbe/p6/GestionCommande/GestionCommandeApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.edu.isepat.dbe.p6.GestionCommande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCommandeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
